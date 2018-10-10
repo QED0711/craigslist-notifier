1a5505040c975ccd7ade3a6d9923ef6f880e3cc8
QED0711 <qdizon@gmail.com>
2018-10-10 15:03:55 -0400

    working post request from searches index page

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -1,5 +1,12 @@
 const getListings = () => {
-    $.get(/* searches show api endpoint here */, function(data){
-        console.log(data)
+    $(".searches").html("")
+    let user = $(".current_user")
+    $.get(`/users/${user.data("id")}/currentsearches.json`, function(data){
+        let template = Handlebars.compile(document.getElementById("card").innerHTML)
+        let results = template(data)
+        
+        $(".searches").html(results)
+        $(".search-info").click(getListings)
     })
 }
+
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -16,10 +16,11 @@ class SearchesController < ApplicationController
   end
 
   def create
-    search = Search.new(search_params)
-    search.user_id = current_user.id
-    search.save
-    binding.pry
+    @search = Search.new(search_params)
+    @search.user_id = current_user.id
+    @search.save
+    render json: @search, status: 201
+    # binding.pry
   end
 
   def edit
