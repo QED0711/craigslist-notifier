f6d0320cf0bb3b4b3bc1213b35d88c69ae9ffd63
QED0711 <qdizon@gmail.com>
2018-10-10 17:45:29 -0400

    added js functions to only add most recent searches when creating a new search from index

--- app/assets/javascripts/requests.js
+++ app/assets/javascripts/requests.js
@@ -10,9 +10,35 @@ const getListings = () => {
     })
 }
 
+const getAllSearches = () => {
+    $(".searches").html("")
+    let user = $(".current_user")
+    $.get(`/users/${user.data("id")}/currentsearches.json`, function(data){
+        let template = Handlebars.compile(document.getElementById("card").innerHTML)
+        let results = template(data)
+        
+        resetSearches(results);
+    })
+}
+
 const clearNewSearchForm = () => {
     $("input[type=submit]").removeAttr('disabled')
     $("#search_url")[0].value = ""
     $("#search_description")[0].value = ""
+}
+
+const resetSearches = (results) => {
+    $(".searches").html(results)
+}
+
+const addRecentSearches = () => {
+    let user = $(".current_user")
+    $.get(`/users/${user.data("id")}/currentsearches.json`, function(data){
+        let template = Handlebars.compile(document.getElementById("card").innerHTML)
+        data = [data[data.length-1]] 
+        let results = template(data)       
+        
+        $(".searches").append(results)
+    }) 
 }
 
